bf05bddacc9a8945e23bdc9bf56cd13b985a152d
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-09-14 10:05:51 -0300

    Added Host

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ app.register(createTranscriptionRoute)
 app.register(generateAiCompletionRoute)
 
 app.listen({
+  host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
   console.log("HTTP Server Running!");
