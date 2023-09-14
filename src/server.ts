e62a0b4dbfd6b988a238f9de07a67af0740b7cc1
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-09-14 10:41:59 -0300

    Added host

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 const app = fastify()
 
 app.register(fastifyCors, {
-  origin: '*'
+  origin: true
 })
 
 app.register(getAllPromptsRoute)
@@ -17,9 +17,11 @@ app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
 app.register(generateAiCompletionRoute)
 
-app.listen({
-  // host: '0.0.0.0',
-  port: process.env.PORT ? Number(process.env.PORT) : 3333,
-}).then(() => {
-  console.log("HTTP Server Running!");
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
